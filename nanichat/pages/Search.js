72337b2ace75b8430d0b79acfa0028bf008ce4f0
sunnyh2024 <sunnyshuang.2020@gmail.com>
2021-02-13 16:24:34 -0500

    pog4

---
+++ nanichat/pages/Search.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from "react";
+import { SafeAreaView, FlatList, StyleSheet, Text, View, Button } from 'react-native';
+
+function Search() {
+    
+}
+
+export default Search;
