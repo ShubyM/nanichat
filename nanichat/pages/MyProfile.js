f9d76da8d34ebc9b9d95a0a2ea797ed679560ebb
he-is-james <he.is.james7@gmail.com>
2021-02-13 00:36:47 -0500

    started navigation

---
+++ nanichat/pages/MyProfile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+
+function MyProfile() {
+    return (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>My Explorer Profile Screen</Text>
+        </View>
+    )
+}
+
+export default MyProfile;
